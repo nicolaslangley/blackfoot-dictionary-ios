88f0a0c5b09c8580b964dd57e99d224b92ab6a72
Nicolas Langley <nicolasqlangley@gmail.com>
2014-08-21 16:38:09 -0700

    Initial Commit

---
+++ Blackfoot Dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blackfoot Dictionary
+//
+//  Created by Nicolas Langley on 8/21/14.
+//  Copyright (c) 2014 Hierarchy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
