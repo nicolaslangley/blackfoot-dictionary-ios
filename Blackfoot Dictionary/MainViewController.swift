7b06a55122a1f5a2a34b1db6a2a6c503e8174868
Nicolas Langley <nicolasqlangley@gmail.com>
2014-08-21 18:09:51 -0700

    Added basic UI elements in Swift

---
+++ Blackfoot Dictionary/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Blackfoot Dictionary
+//
+//  Created by Nicolas Langley on 8/21/14.
+//  Copyright (c) 2014 Hierarchy. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class MainViewController: UIViewController {
+    
+    // Input outlets from Interface Builder
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputTextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = "Blackfoot Dictionary"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        if (segue.identifier == "TranslateSegue") {
+            var vc: TranslateViewController = segue.destinationViewController as TranslateViewController
+            vc.setInputText(inputTextField.text)
+        }
+    }
+
+    // Handle pressing of translate button
+    @IBAction func translateButtonPressed(sender : AnyObject) {
+        self.performSegueWithIdentifier("TranslateSegue", sender: sender)
+    }
+    
+
+}
+
---
+++ Blackfoot Dictionary/TranslateViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TranslateViewController.swift
+//  Blackfoot Dictionary
+//
+//  Created by Nicolas Langley on 8/21/14.
+//  Copyright (c) 2014 Hierarchy. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class TranslateViewController: UIViewController {
+    
+    @IBOutlet weak var outputTextLabel: UILabel!
+    var inputText: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "Translate"
+        // Display the translated text (in this case just a default placeholder)
+        outputTextLabel.text = inputText
+        outputTextLabel.textAlignment = NSTextAlignment.Center
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Receive the input text value and store it
+    func setInputText(input : String) {
+        inputText = input
+    }
+
+    
+    
+    
+}
+
